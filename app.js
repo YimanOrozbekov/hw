ec514c42a4cf10e743e9780e2839d9101e1b0c76
YimanOrozbekov <yimanorozbrkov@gmail.com>
2023-05-10 17:38:07 +0600

    first commit

---
+++ app.js
@@ -0,0 +1,13 @@
+//классы опер...
+//! - не, +, - ;
+//тернарные опер...
+// var age = prompt('enter your age')
+// console.log(age >= 20 && age <= 50 ? 'вы приняты' : 'error' )
+// var num = prompt('введите число')
+// console.log(num > 10  ? 'больше десяти' : 'меньше десятиgit initgit initgit initgit init' )
+var matrix = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+]
+console.log(matrix)
