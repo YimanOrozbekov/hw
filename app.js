a35655c1b6d49ba69f7b7c52a7519d721a095256
Orozbekov Yiman <127510137+YimanOrozbekov@users.noreply.github.com>
2023-05-13 17:18:26 +0600

    Update app.js

--- app.js
+++ app.js
@@ -1,27 +1,20 @@
-
-//  HW_3.1  //
-
-for(var i = 0; i <= 100; i++) {
-    if (i % 5 === 0 && i % 3 === 0) {
-        console.log('fizzBuzz')
-    } else if (i % 5 === 0) {
-        console.log('fizz')
-    } else if (i % 3 === 0) {
-        console.log('fizz')
-    } else {
-        console.log(i)
+var starbucks = {
+    drinks: {
+        coffee:{
+            latte: 'latte',
+            cappuccino: 'cappuccino',
+            americano: 'americano',
+            frappuccino: 'frappuccino'
+        },
+        tea: {
+            black: 'black',
+            green: 'green'
+        }
     }
 }
-
-//HW_3.2
-
-var milkAndBeer = ['milk', 'beer', 'beer', 'milk', 'milk']
-var bad = ['beer']
-for (var i = 0; i < milkAndBeer.length; i++){
-    if(bad.includes(milkAndBeer[i].toLowerCase())){
-        console.log(`${milkAndBeer[i]} BAD`)
-        continue
-    }
-
-    console.log(`${milkAndBeer[i]} GOOD`)
+var drinks = prompt('Выберите напиток : из кофе есть...')
+if(drinks in starbucks.drinks.coffee || drinks in starbucks.drinks.tea){
+    console.log('заказ принят')
+}else{
+    console.log('такого напитка нет')
 }
